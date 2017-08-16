f02a6427a3ac2194316518aa35b1d64a49703d01
kuangzhiqiang [邝志强] <kuangzhiqiang@xiaomi.com>
2017-08-16 10:43:17 +0800

    格式化错误信息

--- application/controllers/Error.php
+++ application/controllers/Error.php
@@ -24,25 +24,21 @@ class ErrorController extends Yaf_Controller_Abstract
 
     public function errorAction($exception)
     {
-        try
-        {
+        try {
             \Logger::getLogger(str_replace('\\', '_', get_class($exception)))->logException($exception);
-        }
-        catch (Exception $e)
-        {
+        } catch (Exception $e) {
             $exception = $e;
         }
 
         $string_msg = get_class($exception) . ": "
-                . $exception->getMessage()
-                . " in file "
-                . $exception->getFile()
-                . " at line "
-                . $exception->getLine() . "\n"
-                . $exception->getTraceAsString() . "\n";
+            . $exception->getMessage()
+            . " in file "
+            . $exception->getFile()
+            . " at line "
+            . $exception->getLine() . "\n"
+            . $exception->getTraceAsString() . "\n";
 
-        if ($this->getRequest()->isCli())
-        {
+        if ($this->getRequest()->isCli()) {
             echo $string_msg;
             goto end;
         }
@@ -50,20 +46,15 @@ class ErrorController extends Yaf_Controller_Abstract
         $this->getView()->setLayout(null);
         $this->getView()->setScriptPath($this->config->application->directory . "/views");
 
-        switch ($exception->getCode())
-        {
+        switch ($exception->getCode()) {
             case YAF_ERR_AUTOLOAD_FAILED:
             case YAF_ERR_NOTFOUND_MODULE:
             case YAF_ERR_NOTFOUND_CONTROLLER:
             case YAF_ERR_NOTFOUND_ACTION:
-                if (!$this->config->application->displayErrors)
-                {
-                    if ($this->getRequest()->isXmlHttpRequest())
-                    {
-                        echo "404 Page Not Found";
-                    }
-                    else
-                    {
+                if (!$this->config->application->displayErrors) {
+                    if ($this->getRequest()->isXmlHttpRequest()) {
+                        $this->responseMessage("404 Page Not Found");
+                    } else {
                         $this->redirect('/error/notFound');
                     }
                     goto end;
@@ -75,11 +66,9 @@ class ErrorController extends Yaf_Controller_Abstract
         }
 
         /* if errors are enabled show the full trace */
-        if ($this->config->application->displayErrors)
-        {
-            if ($this->getRequest()->isXmlHttpRequest())
-            {
-                echo $string_msg;
+        if ($this->config->application->displayErrors) {
+            if ($this->getRequest()->isXmlHttpRequest()) {
+                $this->responseMessage($string_msg);
                 goto end;
             }
 
@@ -92,14 +81,29 @@ class ErrorController extends Yaf_Controller_Abstract
                 "params"         => [] + $params + $this->getRequest()->getQuery() + $this->getRequest()->getPost()
             ];
             $this->display("exception", $assign);
-        }
-        else
-        {
+        } else {
+            if ($this->getRequest()->isXmlHttpRequest()) {
+                $this->responseMessage("service error.");
+                goto end;
+            }
+
             $this->display("error");
         }
 
         end:
-            exit;
+        exit;
+    }
+
+    /**
+     * 返回错误信息
+     * @param $message
+     */
+    private function responseMessage($message)
+    {
+        echo $this->getRequest()->isPost() ? json_encode([
+            'code'    => ErrorCode::SYS_FAILED,
+            'message' => $message,
+        ]) : $message;
     }
 
     /**
